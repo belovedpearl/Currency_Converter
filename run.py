7b7a1b70dcffbe63af0e0954847daa72acdd3b23
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-05-28 21:40:57 +0000

    Add function to start the programme

--- run.py
+++ run.py
@@ -19,6 +19,31 @@ COLORS = {\
     "black_background": "\u001b[40m",
 }
 
+def start_app():
+    currencies = get_currencies() 
+    print("Press 1 to list the available currencies.\n")
+    print("Press 2 to convert from one currency to another\n")
+    print("Press 3 to get exchange rate of two currencies.\n\n")
+    while True:
+        answer = input("Choose an option or press q to quit.")
+
+        if answer == "q":
+            break
+        elif answer == "1":
+            list_currencies(currencies)
+        elif answer == "2":
+            currency1 = input("Enter your base currency id.").upper()
+            currency2 = input("What currency id are you converting to?").upper()
+            amount = input(f"Enter an amount in {currency1}.").upper()
+            convert_currencies(currency1, currency2, amount)
+        elif answer == "3":
+            currency1 = input("Enter a base currency id.").upper()
+            currency2 = input("Enter the next currency id").upper()
+            exchange_rate(currency1, currency2)
+        else:
+            print("You have made an invalid choice.")
+            
+
 def colorRep(text):
     """
     Coordinate color rendering 
