4cea1085442eae77d2fe222cb1f4bd08335211a9
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-06-02 12:37:42 +0000

    Edit config.py to json file and modify access to the key

--- run.py
+++ run.py
@@ -1,12 +1,18 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-import config  # file containing the API key
+
 import os
 from requests import get
 from regulator import *
 import gspread
 from google.oauth2.service_account import Credentials
+import json
+
+with open("config.json","r") as details:
+    file = details.read()
+    content = json.loads(file)
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -24,7 +30,8 @@ SHEET = GSPREAD_CLIENT.open("currency_converter")
 BASE_URL = "https://free.currconv.com/"
 
 # Get APIkey contained in the config file
-KEY = config.API_KEY
+#KEY = config.API_KEY
+KEY = content['API_KEY']
 
 
 def get_countries(currencies):
