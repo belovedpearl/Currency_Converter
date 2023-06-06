1f3ebe75862913abdf7696eaceb5c986a47d4879
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-06-06 03:40:39 +0000

    Modify color use across display

--- run.py
+++ run.py
@@ -20,7 +20,6 @@ SCOPE = [
 ]
 
 CREDS = Credentials.from_service_account_info(json.loads(os.environ["CREDS"]))
-# CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("currency_converter")
@@ -29,9 +28,7 @@ SHEET = GSPREAD_CLIENT.open("currency_converter")
 # URL for the get request
 BASE_URL = "https://free.currconv.com/"
 
-# Get APIkey contained in the config file
-#KEY = config.API_KEY
-# KEY = content['API_KEY']
+# Get API key saved in the var environment
 KEY = os.environ["API_KEY"]
 
 
@@ -105,12 +102,12 @@ def list_currencies(currencies):
     """
     List the returned currencies
     """
-    typewriter(colorRep("Column 1 displays [[red]]IDENTITY[[black]] of currencies.\n"))
+    typewriter(colorRep("Column 1 displays [[red]]IDENTITY[[white]] of currencies.\n"))
     typewriter(
-        colorRep("Column 2 displays [[red]]NAMES[[black]] of currencies.\n")
+        colorRep("Column 2 displays [[red]]NAMES[[white]] of currencies.\n")
     )
     typewriter(
-        colorRep("Column 3 displays [[red]]SYMBOLS[[black]] of the currencies.\n")
+        colorRep("Column 3 displays [[red]]SYMBOLS[[white]] of the currencies.\n")
     )
 
     # loop through the returned tuple
@@ -122,7 +119,7 @@ def list_currencies(currencies):
             "currencySymbol", ""
         )  # Return value if found and "" if not existing
         print(
-            colorRep(f"[[blue]]{identity}[[black]] - {name} - [[red]]{symbol}[[black]]")
+            colorRep(f"[[blue]]{identity}[[white]] - {name} - [[red]]{symbol}[[white]]")
         )
 
 
@@ -147,44 +144,45 @@ def get_currencies():
 
 def start_app():
     """
+    Welcomes the user
     Prompts user to make a choice
     Continues the program in the path chosen
     """
     currencies = get_currencies()
 
-    print("Welcome to MyCurrency...\n")
-    typewriter(colorRep("[[blue]]The Currency Master.\n[[black]]"))
+    print("Welcome to MyCash...\n")
+    typewriter(colorRep("[[blue]]The Currency Master.\n[[white]]"))
     print("What will you like to do today?\n")
 
     typewriter(
         colorRep(
-            "Press [[red]]1[[black]] to [[red]]LIST[[black]] the available currencies.\n"
+            "Press [[red]]1[[white]] to [[red]]LIST[[white]] the available currencies.\n"
         )
     )
     typewriter(
         colorRep(
-            "Press [[red]]2[[black]] to [[red]]CONVERT[[black]] from one currency to another.\n"
+            "Press [[red]]2[[white]] to [[red]]CONVERT[[white]] from one currency to another.\n"
         )
     )
     typewriter(
         colorRep(
-            "Press [[red]]3[[black]] to get [[red]]EXCHANGE[[black]] rate of two currencies.\n"
+            "Press [[red]]3[[white]] to get [[red]]EXCHANGE[[white]] rate of two currencies.\n"
         )
     )
     typewriter(
         colorRep(
-            "Press [[red]]4[[black]] to [[red]]GET[[black]] a list of available countries.\n\n"
+            "Press [[red]]4[[white]] to [[red]]GET[[white]] a list of available countries.\n\n"
         )
     )
 
     while True:
         answer = input(
-            colorRep("Choose an option or press [[red]]q[[black]] to quit.\n")
+            colorRep("Choose an option or press [[red]]q[[white]] to quit.\n")
         )
 
         if answer == "q":
-            typewriter("Thank you for using MyCurrency...\n")
-            typewriter(colorRep("[[blue]]SEE YOU NEXT TIME.[[black]]\n"))
+            typewriter("Thank you for using MyCash...\n")
+            typewriter(colorRep("[[blue]]SEE YOU NEXT TIME.[[white]]\n"))
             break
         elif answer == "1":
             clear_screen()
@@ -211,9 +209,6 @@ def register_user(name):
     """
     Update the register with the new username
     """
-    # print("\nSignup below: ")
-    # username = input("Enter your Username: \n")
-    # print(username)
     worksheet_to_update = SHEET.worksheet("register")
 
     # Convert the username to a list
@@ -221,6 +216,7 @@ def register_user(name):
     # Update the username to the end of the column
     worksheet_to_update.append_row(add_to_list)
     typewriter("in Progress...\n")
+    typewriter("...\n")
     print(f"You are now a registered user.\n")
     start_app()
 
@@ -252,9 +248,9 @@ def sign_in():
     Takes in username
     calls to check if username is registered
     """
-    username = input(colorRep(
-        "[[white]]Enter your username: \n[[black]]"
-    ))  # Retained as user types it in for proper verification of user
+    username = input(
+        "Enter your username: \n"
+    )  # Retained as user types it in for proper verification of user
     check_status(username)
 
 
